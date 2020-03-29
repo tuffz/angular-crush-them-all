de8260232f4d867228dca4346bf0e61bd01b9008
Eric Büttner <eric.buettner@tuffz.com>
2020-03-29 12:14:48 +0200

    :recycle: Observable HeroService and subscribe in HeroesComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from "rxjs";
 
 import { Hero } from "./hero.interface";
 import { HEROES } from "./mock-heroes";
@@ -11,7 +12,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
+
 }
