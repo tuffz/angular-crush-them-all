44d8d4494fc436c37ce1cdb6028b363b062d82a3
Eric Büttner <eric.buettner@tuffz.com>
2020-03-29 11:35:57 +0200

    :truck: Move heroes mocks from feature component to app root

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from "../hero.interface";
-import { HEROES } from "./mocks/heroes";
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
