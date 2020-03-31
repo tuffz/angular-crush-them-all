502db57f3707c139b6d0491307adcdc51f1e3848
Eric Büttner <eric.buettner@tuffz.com>
2020-03-31 23:17:38 +0200

    :construction: Send a message from HeroService
    Reference: https://angular.io/tutorial/toh-pt4#send-a-message-from-heroservice

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,8 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
