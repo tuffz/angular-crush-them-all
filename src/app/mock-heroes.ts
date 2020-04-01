48f9edf2ee44f4cdc1d89a17becd6926d14d10fb
Eric Büttner <eric.buettner@tuffz.com>
2020-04-01 05:43:54 +0200

    :seedling: Extend heroes seed with all common heroes
    Extend heroes seed with all currently available "Crush Them All" common heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,10 +3,86 @@ import { Hero } from './hero.interface';
 export const HEROES: Hero[] = [
   {
     id: 1,
-    name: 'Luka',
+    name: 'Blue Fish',
   },
   {
     id: 2,
+    name: 'Bugonaut Fighter',
+  },
+  {
+    id: 3,
+    name: 'Bugonaut Archer',
+  },
+  {
+    id: 4,
+    name: 'Bugonaut Giant',
+  },
+  {
+    id: 5,
+    name: 'Bugonaut Spear',
+  },
+  {
+    id: 6,
+    name: 'Vulcan Archer',
+  },
+  {
+    id: 7,
+    name: 'Tiny Dragon',
+  },
+  {
+    id: 8,
+    name: 'Vulcan Fighter',
+  },
+  {
+    id: 9,
+    name: 'Vulcan Hammer',
+  },
+  {
+    id: 10,
+    name: 'Voodoo Spear',
+  },
+  {
+    id: 11,
+    name: 'Voodoo Archer',
+  },
+  {
+    id: 12,
+    name: 'Sprout',
+  },
+  {
+    id: 13,
+    name: 'Voodoo Dagger',
+  },
+  {
+    id: 14,
+    name: 'Light Knight',
+  },
+  {
+    id: 15,
+    name: 'Swift',
+  },
+  {
+    id: 16,
+    name: 'Big Eye',
+  },
+  {
+    id: 17,
+    name: 'Spike',
+  },
+  {
+    id: 18,
+    name: 'Skeleton Infantry',
+  },
+  {
+    id: 19,
     name: 'Bat',
+  },
+  {
+    id: 20,
+    name: 'Skeleton Ranger',
+  },
+  {
+    id: 21,
+    name: 'Skeleton Giant',
   },
 ];
