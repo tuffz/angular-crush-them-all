12c56948ecb27547de1ce36f9819a4dc515ac6b0
Eric Büttner <eric.buettner@tuffz.com>
2020-04-01 02:07:19 +0200

    :sparkles: Add additional message onSelect in  HeroesComponent
    References: https://angular.io/tutorial/toh-pt4#add-additional-messages-to-hero-service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero.interface';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +15,11 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {
+  }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -22,6 +27,8 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService
+      .add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
