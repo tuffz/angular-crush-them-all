18786404f50e7d1e52dfcff7be2f02c40a1cfc6c
Eric Büttner <eric.buettner@tuffz.com>
2020-04-10 18:18:05 +0200

    :art: Format "HeroService.getHeroes()"

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,7 +18,10 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
+    this.messageService
+      .add('HeroService: fetched heroes')
+      ;
+
     return of(HEROES);
   }
 
