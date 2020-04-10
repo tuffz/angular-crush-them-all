fb6a6b29e245b88434aa2db4dfc760f0f7428ad4
Eric Büttner <eric.buettner@tuffz.com>
2020-04-10 17:38:16 +0200

    :fire: Remove dead code
    Reference: https://angular.io/tutorial/toh-pt5#remove-dead-code-optional

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero.interface';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,23 +12,13 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
   ) {
   }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService
-      .add(`HeroService: Selected hero id=${hero.id}`)
-      ;
   }
 
   getHeroes(): void {
