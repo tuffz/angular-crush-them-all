eb72fb29c74e086eb71fc5557ac5d6174d09e517
Eric Büttner <eric.buettner@tuffz.com>
2020-04-10 17:10:11 +0200

    :sparkles: Add "hero detail" route
    Reference: https://angular.io/tutorial/toh-pt5#add-a-hero-detail-route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { GatewayComponent } from './gateway/gateway.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/gateway', pathMatch: 'full' },
   { path: 'gateway', component: GatewayComponent },
+  { path: 'datail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
