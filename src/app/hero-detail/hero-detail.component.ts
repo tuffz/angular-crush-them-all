b3dfa2d2529b64f94cdc0b74889dac7d9f1c34ce
Eric Büttner <eric.buettner@tuffz.com>
2020-04-10 18:13:42 +0200

    :construction: Extract "id" route parameter
    Reference: https://angular.io/tutorial/toh-pt5#extract-the-id-route-parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -20,6 +20,15 @@ export class HeroDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService
+      .getHero(id)
+      .subscribe(hero => this.hero = hero)
+      ;
   }
 
 }
